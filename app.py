89580634f0ca8de83e422bcfa3bf3c86e41083d2
integrador603-jpg <integrador603@gmail.com>
2026-07-29 22:37:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ def analizar_saturacion():
     if "png" in img_b64[:30]: media_type = "image/png"
     elif "webp" in img_b64[:30]: media_type = "image/webp"
 
-    api_key = os.environ.get("GEMINI_API_KEY", "")
+    api_key = os.environ.get("GAQ.Ab8RN6Lxp2OO3d9hlNhHTH94_gyj2y0851sXpx2AtGF4adtn_w", "")
     if not api_key:
         return jsonify({"error": "GEMINI_API_KEY no configurada"}), 500
 
